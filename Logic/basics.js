9743fe3146db8f81a0d2225fa351d9fcb2be2ace
Fatahillah <fatahillah@yandex.com>
2023-10-12 00:01:00 +0700

    structure changed  for basics

--- Logic/basics.js
+++ Logic/basics.js
@@ -1,24 +1,5 @@
 // All solutions without built-in function; language used: Indonesian or English
 
-/* 
-1. Diketahui di dalam sebuah perusahaan Z total bersih gaji karyawan dihitung dari gaji pokok 
-   sesuai posisi, ditambah tunjangan karyawan dan dikurangi angsuran hutang ke perusahaan.
-
-   Gaji bersih = gaji pokok + tunjangan - hutang
-
-   Setiap posisi memiliki gaji pokok yang berbeda:
-   	-Frontend Developer = 11_000_000
-	-Backend Developer = 10_000_000
-   
-   Tunjangan karyawan hanya diberikan kepada karyawan yang sudah menikah sebesar 300_000
-
-   Apabila karyawan sudah memiliki anak, maka tiap anak akan diberikan tunjangan sebesar 100_000
-
-   Q: Buatlah algoritma yang akan menentukan total gaji bersih karyawan yang diterima karyawan 
-      setelah dikurangi angsuran hutang, dan analisa variabel apa saja yang dibutuhkan. Setelah 
-      membuat algoritma, buatlah program berdasarkan algoritma yang telah dibuat!
-*/
-
 // Ans_1:
 let name = "Max";
 let position = "Backend Developer";
